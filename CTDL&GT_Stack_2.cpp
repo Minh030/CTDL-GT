5cc7641eb39eb636698d082c1bd885c52e798bd8
Minh030 <125718653+Minh030@users.noreply.github.com>
2023-04-07 11:19:04 +0700

    Update CTDL&GT_Stack_2.cpp

--- CTDL&GT_Stack_2.cpp
+++ CTDL&GT_Stack_2.cpp
@@ -1,21 +1,74 @@
+Nguon : https://vietjack.com/cau-truc-du-lieu-va-giai-thuat/ngan-xep-trong-c.jsp
+
+#include <stdio.h>
+
+int MAXSIZE = 8;       
+int stack[8];     
+int top = -1;            
+
+int isempty() {
+
+   if(top == -1)
+      return 1;
+   else
+      return 0;
+}
+   
+int isfull() {
+
+   if(top == MAXSIZE)
+      return 1;
+   else
+      return 0;
+}
+
+int peek() {
+   return stack[top];
+}
+
+
+int pop() {
+   int data;
+	
+   if(!isempty()) {
+      data = stack[top];
+      top = top - 1;   
+      return data;
+   }else {
+      printf("Khong the thu thap du lieu, ngan xep (Stack) la trong.\n");
+   }
+}
+
+int push(int data) {
+
+   if(!isfull()) {
+      top = top + 1;   
+      stack[top] = data;
+   }else {
+      printf("Khong the chen du lieu, ngan xep (Stack) da day.\n");
+   }
+}
+
 int main() {
-    Stack s; // Khởi tạo đối tượng Stack
+   // chen cac phan tu vao ngan xep
+   push(3);
+   push(5);
+   push(9);
+   push(1);
+   push(12);
+   push(15);
 
-    init(&s); // Khởi tạo stack rỗng
+   printf("Phan tu tai vi tri tren cung cua ngan xep: %d\n" ,peek());
+   printf("Cac phan tu: \n");
 
-    push(&s, 1);
-    push(&s, 2);
-    push(&s, 3);
+   // in cac phan tu trong ngan xep
+   while(!isempty()) {
+      int data = pop();
+      printf("%d\n",data);
+   }
 
-    printf("Top of stack: %d\n", getTop(&s)); // In giá trị phần tử đầu trong stack
-    pop(&s); // Xóa phần tử đầu trong stack
-    printf("Top of stack after pop: %d\n", getTop(&s)); // In giá trị phần tử đầu trong stack sau khi pop
-
-    if (isEmpty(&s)) {
-        printf("Stack rong!\n"); // Kiểm tra stack có trống hay không
-    } else {
-        printf("Stack khong rong!\n");
-    }
-
-    return 0;
+   printf("Ngan xep da day: %s\n" , isfull()?"true":"false");
+   printf("Ngan xep la trong: %s\n" , isempty()?"true":"false");
+   
+   return 0;
 }
